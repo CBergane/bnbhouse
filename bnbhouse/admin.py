7641470f470efc75058370afe4358486d6d8c9e8
CBergane <christian.bergane@gmail.com>
2022-12-14 11:37:47 +0000

    added function to add houses and whats inside, and for guest to add a booing

--- bnbhouse/admin.py
+++ bnbhouse/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import house, Bookings
 
-# Register your models here.
+
+admin.site.register(house)
+admin.site.register(Bookings)
--- bnbhouse/models.py
+++ bnbhouse/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.urls import path
+from django.conf import settings
 
-# Create your models here.
+
+class house(models.Model):
+    HOUSE_CATAGORIES = (
+        ('BAS', 'BASIC'),
+        ('STN', 'STANDARD'),
+        ('LUX', 'LUXURIUS'),
+        ('ROY', 'ROYAL'),
+    )
+    number = models.IntegerField()
+    catagory = models.CharField(max_length=3, choices=HOUSE_CATAGORIES)
+    beds = models.IntegerField()
+    capacity = models.IntegerField()
+    description = models.TextField(max_length=500)
+
+    def __str__(self):
+        return f'{self.number}. {self.catagory} with {self.beds} beds for {self.capacity} people'
+
+
+class Bookings(models.Model):
+    guest = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    house = models.ForeignKey(house, on_delete=models.CASCADE)
+    check_in = models.DateField()
+    check_out = models.DateField()
+
+    def __str__(self):
+        return f'{self.guest} has booked {self.house} from {self.check_in} untill {self.check_out}'
