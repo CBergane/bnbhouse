b80caa5af8d72f90669931af80043170ae6916c8
CBergane <christian.bergane@gmail.com>
2022-12-21 17:37:36 +0000

    linked register, sign in and out to main base.html

--- airbnb/urls.py
+++ airbnb/urls.py
@@ -24,5 +24,4 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('', include('bnbhouse.urls')),
     path('accounts/', include('allauth.urls')),
-    path('accounts/', include('allauth.urls')),
 ]
